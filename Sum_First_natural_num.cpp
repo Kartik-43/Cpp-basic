a06ff14043e8549df34d47c3c0833bc39090b983
Kartik-43 <144028477+Kartik-43@users.noreply.github.com>
2024-08-18 14:17:09 +0530

    Sum_First_natural_num.cpp

--- Sum_First_natural_num.cpp
+++ Sum_First_natural_num.cpp
@@ -1,15 +1,78 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-    int n, sum = 0;
-    cout << "Enter a positive integer: ";
-    cin >> n;
+
+int firstN(int n)
+{
+    int sum = 0;
 
     for(int i = 1; i <= n; ++i) {
         sum += i;
     }
 
-    cout << "Sum = " << sum;
+    return sum;
+}
+
+
+int firstEven(int n)
+{
+    int sum = 0;
+
+    for(int i = 1; i <= n; ++i) {
+        sum += 2 * i;
+    }
+
+    return sum;
+}
+
+
+int firstOdd(int n)
+{
+    int sum = 0;
+
+    for(int i = 1; i <= n; ++i) {
+        sum += ( ( 2 * i ) - 1 );
+    }
+
+    return sum;
+}
+
+
+int main() 
+{
+    int n, ch, sum;
+
+    cout << "\n\nEnter a positive integer : ";
+    cin >> n;
+
+    cout << "\nWhat do you want to calculate ( 1 / 2 / 3 ) :- " << endl;
+    cout << "1) SUM of first 'n' Natural Numbers " << endl;
+    cout << "2) SUM of first 'n' Even Natural Numbers " << endl;
+    cout << "2) SUM of first 'n' Odd Natural Numbers " << endl;
+
+    cin >> ch;
+
+    if (ch == 1)
+    {
+        sum = firstN(n);
+        cout << "The sum of first " << n << " Natural numbers is " << endl;
+        cout << sum;
+    }
+        
+    else if (ch == 2)
+    {
+        sum = firstEven(n);
+        cout << "The sum of first " << n << " Even Natural numbers is " << endl;
+        cout << sum;
+    }
+        
+    else if (ch == 3)
+    {
+        sum = firstOdd(n);
+        cout << "The sum of first " << n << " Odd Natural numbers is " << endl;
+        cout << sum;
+    }
+
     return 0;
 }
